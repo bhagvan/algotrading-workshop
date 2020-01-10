dd9124adb6b8c67657fb52cbae702ebb07310ba1
EC2 Default User <ec2-user@ip-172-16-158-88.ec2.internal>
2020-01-10 22:41:02 +0000

    update for url

--- 2_Strategies/update_config.py
+++ 2_Strategies/update_config.py
@@ -15,6 +15,15 @@ config["algo_name"] = algo_name
 with open("model/algo_name", "w") as text_file:
     text_file.write(algo_name)
 
+try:
+    s3 = boto3.client('s3')
+    s3.download_file('ee-assets-prod-us-east-1', 'modules/7c974653c2164bcab350d902744088c5/v1/algo_event.config', 'algo_event.config')
+    with open('algo_event.config', 'r') as f:
+        event_config = json.load(f)
+    config['submitUrl']=event_config['submitUrl']
+except:
+  print("Skipped event config") 
+    
 with open("model/algo_config", "w") as text_file:
     text_file.write(json.dumps(config))    
 
